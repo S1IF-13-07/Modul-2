49fcada5c8eed9c7f3a518e3f01d54d593e15923
rafioktars <152888181+rafioktars@users.noreply.github.com>
2025-09-30 05:05:08 +0700

    Create soal3.go

---
+++ Rafi Oktarino Ramafhan/soal3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var r float64 
+
+    fmt.Print("Masukkan jari-jari lingkaran: ")
+    fmt.Scanln(&r)
+
+    luas := math.Pi * r * r
+
+    fmt.Printf("Luas lingkaran dengan jari-jari %.1f adalah %.1f\n", r, luas)
+}
