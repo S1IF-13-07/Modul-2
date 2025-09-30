67d6f1d4dd9c714fbd21dacf00b32878e7956697
NasgorEnjoyer <95670960+NasgorEnjoyer@users.noreply.github.com>
2025-10-01 04:19:12 +0700

    Add files via upload

---
+++ M MAHDAN ARGYA SYARIF/TUGAS PENDAHULUAN/tp.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string
+	fmt.Print("Masukkan nama kalian: ")
+	fmt.Scanln(&nama)
+
+	fmt.Println("Halo,", nama)
+}
+
+// M MAHDAN ARGYA SYARIF
+// 109082500059
+// IF-13-07
